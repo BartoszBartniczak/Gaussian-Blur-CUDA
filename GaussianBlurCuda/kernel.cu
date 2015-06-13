15be1a1258be8d0091be37912a53d9dbc1f4fb96
Bartosz Bartniczak <bartoszbartniczak@gmail.com>
2015-06-13 20:02:36 +0200

    Boki
    Działa

--- GaussianBlurCuda/kernel.cu
+++ GaussianBlurCuda/kernel.cu
@@ -92,7 +92,7 @@ void header(){
 __global__ void GaussianBlur(int *B, int *G, int *R, int numberOfPixels, int width, int *B_new, int *G_new, int *R_new)
 {
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	
+
 	int mask[] = { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
 	int s = mask[0] + mask[1] + mask[2] + mask[3] + mask[4] + mask[5] + mask[6] + mask[7] + mask[8];
 
@@ -112,34 +112,56 @@ __global__ void GaussianBlur(int *B, int *G, int *R, int numberOfPixels, int wid
 			R_new[index] = (R[index] * mask[4] + R[index + width - 1] * mask[0] + R[index + width] * mask[1] + R[index - 1] * mask[3]);
 			return;
 		}
+		//reszta pikseli w dolnym rzedzie
+		s = mask[4] + mask[1] + mask[2] + mask[5] + mask[0] + mask[3];
+		B_new[index] = (int)((B[index] * mask[4] + B[index + width] * mask[1] + B[index + width + 1] * mask[2] + B[index + 1] * mask[5] + B[index + width - 1] * mask[0] + B[index - 1] * mask[3]) / s);
+		R_new[index] = (int)((R[index] * mask[4] + R[index + width] * mask[1] + R[index + width + 1] * mask[2] + R[index + 1] * mask[5] + R[index + width - 1] * mask[0] + R[index - 1] * mask[3]) / s);
+		G_new[index] = (int)((G[index] * mask[4] + G[index + width] * mask[1] + G[index + width + 1] * mask[2] + G[index + 1] * mask[5] + G[index + width - 1] * mask[0] + G[index - 1] * mask[3]) / s);
 
-		B_new[index] = B[index];
-		G_new[index] = G[index];
-		R_new[index] = R[index];
 		return;
 	}
-	if (index > numberOfPixels - width){ //gorny rzad pikseli
-		B_new[index] = B[index];
-		G_new[index] = G[index];
-		R_new[index] = R[index];
+	if (index >= numberOfPixels - width){ //gorny rzad pikseli
+
+		if (index == numberOfPixels - width){ //lewy gorny rog
+			s = mask[4] + mask[5] + mask[7] + mask[8];
+			B_new[index] = (int)((B[index] * mask[4] + B[index + 1] * mask[5] + B[index - width] * mask[7] + B[index - width + 1] * mask[8]) / s);
+			G_new[index] = (int)((G[index] * mask[4] + G[index + 1] * mask[5] + G[index - width] * mask[7] + G[index - width + 1] * mask[8]) / s);
+			R_new[index] = (int)((R[index] * mask[4] + R[index + 1] * mask[5] + R[index - width] * mask[7] + R[index - width + 1] * mask[8]) / s);
+			return;
+		}
+
+		if (index == numberOfPixels - 1){ //prawy gorny rog
+			s = mask[4] + mask[3] + mask[6] + mask[7];
+			B_new[index] = (int)((B[index] * mask[4] + B[index - 1] * mask[3] + B[index - width - 1] * mask[6] + B[index - width] * mask[7]) / s);
+			G_new[index] = (int)((G[index] * mask[4] + G[index - 1] * mask[3] + G[index - width - 1] * mask[6] + G[index - width] * mask[7]) / s);
+			R_new[index] = (int)((R[index] * mask[4] + R[index - 1] * mask[3] + R[index - width - 1] * mask[6] + R[index - width] * mask[7]) / s);
+			return;
+		}
+
+		s = mask[4] + mask[3] + mask[5] + mask[6] + mask[7] + mask[8];
+		B_new[index] = (int)((B[index] * mask[4] + B[index - 1] * mask[3] + B[index - width - 1] * mask[6] + B[index - width] * mask[7] + B[index + 1] * mask[5] + B[index - width] * mask[8]) / s);
+		R_new[index] = (int)((R[index] * mask[4] + R[index - 1] * mask[3] + R[index - width - 1] * mask[6] + R[index - width] * mask[7] + R[index + 1] * mask[5] + R[index - width] * mask[8]) / s);
+		G_new[index] = (int)((G[index] * mask[4] + G[index - 1] * mask[3] + G[index - width - 1] * mask[6] + G[index - width] * mask[7] + G[index + 1] * mask[5] + G[index - width] * mask[8]) / s);
 		return;
 	}
 	if (index % width == 0){ //lewa sciana
-		B_new[index] = B[index];
-		G_new[index] = G[index];
-		R_new[index] = R[index];
+		s = mask[4] + mask[1] + mask[2] + mask[5] + mask[8] + mask[7];
+		B_new[index] = (int)((B[index] * mask[4] + B[index + width] * mask[1] + B[index + width + 1] * mask[2] + B[index + 1] * mask[5] + B[index - width + 1] * mask[8] + B[index - width]) / s);
+		G_new[index] = (int)((G[index] * mask[4] + G[index + width] * mask[1] + G[index + width + 1] * mask[2] + G[index + 1] * mask[5] + G[index - width + 1] * mask[8] + G[index - width]) / s);
+		R_new[index] = (int)((R[index] * mask[4] + R[index + width] * mask[1] + R[index + width + 1] * mask[2] + R[index + 1] * mask[5] + R[index - width + 1] * mask[8] + R[index - width]) / s);
 		return;
 	}
 	if (index % width == width - 1){ //prawa sciana
-		B_new[index] = B[index];
-		G_new[index] = G[index];
-		R_new[index] = R[index];
+		s = mask[4] + mask[1] + mask[0] + mask[3] + mask[6] + mask[7];
+		B_new[index] = (int)((B[index] * mask[4] + B[index + width] * mask[1] + B[index + width - 1] * mask[0] + B[index - 1] * mask[3] + B[index - width - 1] * mask[6] + B[index - width] * mask[7]) / s);
+		R_new[index] = (int)((R[index] * mask[4] + R[index + width] * mask[1] + R[index + width - 1] * mask[0] + R[index - 1] * mask[3] + R[index - width - 1] * mask[6] + R[index - width] * mask[7]) / s);
+		G_new[index] = (int)((G[index] * mask[4] + G[index + width] * mask[1] + G[index + width - 1] * mask[0] + G[index - 1] * mask[3] + G[index - width - 1] * mask[6] + G[index - width] * mask[7]) / s);
 		return;
 	}
-	
+
 	if (s != 0 && index < numberOfPixels)
 	{
-		
+
 
 		int poz_1 = index - width - 1;
 		int poz_2 = index - width;
@@ -151,9 +173,9 @@ __global__ void GaussianBlur(int *B, int *G, int *R, int numberOfPixels, int wid
 		int poz_8 = index + width;
 		int poz_9 = index + width + 1;
 
-		B_new[index] =  (int)( ((B[poz_1] * mask[0]) + (B[poz_2] * mask[1]) + (B[poz_3] * mask[2]) + (B[poz_4] * mask[3]) + (B[poz_5] * mask[4]) + (B[poz_6] * mask[5]) + (B[poz_7] * mask[6]) + (B[poz_8] * mask[7]) + (B[poz_9] * mask[8])) /s);
-		G_new[index] = (int)( ((G[poz_1] * mask[0]) + (G[poz_2] * mask[1]) + (G[poz_3] * mask[2]) + (G[poz_4] * mask[3]) + (G[poz_5] * mask[4]) + (G[poz_6] * mask[5]) + (G[poz_7] * mask[6]) + (G[poz_8] * mask[7]) + (G[poz_9] * mask[8])) /s );
-		R_new[index] = (int)( ((R[poz_1] * mask[0]) + (R[poz_2] * mask[1]) + (R[poz_3] * mask[2]) + (R[poz_4] * mask[3]) + (R[poz_5] * mask[4]) + (R[poz_6] * mask[5]) + (R[poz_7] * mask[6]) + (R[poz_8] * mask[7]) + (R[poz_9] * mask[8])) /s);
+		B_new[index] = (int)(((B[poz_1] * mask[0]) + (B[poz_2] * mask[1]) + (B[poz_3] * mask[2]) + (B[poz_4] * mask[3]) + (B[poz_5] * mask[4]) + (B[poz_6] * mask[5]) + (B[poz_7] * mask[6]) + (B[poz_8] * mask[7]) + (B[poz_9] * mask[8])) / s);
+		G_new[index] = (int)(((G[poz_1] * mask[0]) + (G[poz_2] * mask[1]) + (G[poz_3] * mask[2]) + (G[poz_4] * mask[3]) + (G[poz_5] * mask[4]) + (G[poz_6] * mask[5]) + (G[poz_7] * mask[6]) + (G[poz_8] * mask[7]) + (G[poz_9] * mask[8])) / s);
+		R_new[index] = (int)(((R[poz_1] * mask[0]) + (R[poz_2] * mask[1]) + (R[poz_3] * mask[2]) + (R[poz_4] * mask[3]) + (R[poz_5] * mask[4]) + (R[poz_6] * mask[5]) + (R[poz_7] * mask[6]) + (R[poz_8] * mask[7]) + (R[poz_9] * mask[8])) / s);
 	}
 
 }
@@ -165,7 +187,7 @@ int main()
 
 	char z;
 
-	int i;	
+	int i;
 
 	// deklaracja zmiennych
 	int *B, *G, *R;
@@ -175,25 +197,25 @@ int main()
 	G = new int[liczba_pikseli*sizeof(int)];
 	R = new int[liczba_pikseli*sizeof(int)];
 
-	
+
 	cout << "--------------" << endl;
 	fseek(forg, 0, SEEK_SET);
-	for (int i = 0; i<File.bfOffBits; i++)
+	for (int i = 0; i < File.bfOffBits; i++)
 	{
 		z = fgetc(forg);
 		fprintf(fsz, "%c", z);                   //Utworzenie naglowka nowej Bitmapy
 	}
-	
+
 	int licznik_znakow = 0;
-	for (int i = File.bfOffBits; i < File.bfOffBits+liczba_pikseli; i++) //wczytanie pikseli
+	for (int i = File.bfOffBits; i < File.bfOffBits + liczba_pikseli; i++) //wczytanie pikseli
 	{
 		int index = i - File.bfOffBits;
 
 		B[index] = (int)(fgetc(forg));
 		G[index] = (int)(fgetc(forg));
 		R[index] = (int)(fgetc(forg));
-		licznik_znakow +=3;
-		if (licznik_znakow == 3*Picture.biWidth){
+		licznik_znakow += 3;
+		if (licznik_znakow == 3 * Picture.biWidth){
 			fgetc(forg);
 			fgetc(forg);
 			licznik_znakow = 0;
@@ -203,7 +225,7 @@ int main()
 
 	cudaError_t cudaStatus = GaussianBlurWithCuda(B, G, R, liczba_pikseli, Picture.biWidth);
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addWithCuda failed!\n");
+		fprintf(stderr, "GaussianBlurWithCuda failed!\n");
 		return 1;
 	}
 
@@ -265,7 +287,7 @@ cudaError_t GaussianBlurWithCuda(int *b, int *g, int *r, long size, int width)
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
 	}
-	
+
 	cudaStatus = cudaMalloc((void**)&d_B_new, size * sizeof(int));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
@@ -321,7 +343,7 @@ cudaError_t GaussianBlurWithCuda(int *b, int *g, int *r, long size, int width)
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		fprintf(stderr, "GaussianBlur launch failed: %s\n", cudaGetErrorString(cudaStatus));
 		goto Error;
 	}
 
@@ -329,7 +351,7 @@ cudaError_t GaussianBlurWithCuda(int *b, int *g, int *r, long size, int width)
 	// any errors encountered during the launch.
 	cudaStatus = cudaDeviceSynchronize();
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching GaussianBlur!\n", cudaStatus);
 		goto Error;
 	}
 
